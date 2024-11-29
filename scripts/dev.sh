206450efe88b84afc61d843cd5835e6d19e56906
Germán Fernández <703573+germanftorres@users.noreply.github.com>
2024-11-29 11:38:08 +0100

    fix tailwindcss config

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build hugo debug
-HUGO_MODULE_WORKSPACE=hugo.work hugo server  --cleanDestinationDir --ignoreCache --debug  --templateMetrics --templateMetricsHints --printUnusedTemplates
+HUGO_MODULE_WORKSPACE=hugo.work hugo server  --cleanDestinationDir --ignoreCache --logLevel info # --templateMetrics --templateMetricsHints --printUnusedTemplates
 
 # build hugo
 # HUGO_MODULE_WORKSPACE=hugo.work hugo server  --cleanDestinationDir
