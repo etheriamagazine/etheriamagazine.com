eef5735ba13f34868e40aff6197d69d629260d7f
germanftorres <703573+germanftorres@users.noreply.github.com>
2025-04-04 16:15:53 +0200

    fix trailing slash, add basicAuth for testing

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/bun';
+import { basicAuth } from 'hono/basic-auth';
 
 import { plausibleProxy } from './plausible';
 import newsletter from './newsletter';
@@ -13,15 +14,26 @@ const port = parseInt(process.env['PORT'] || '') || 3000;
 
 const app = new Hono();
 
+// dev auth
+app.use(
+  '/*',
+  basicAuth({
+    username: 'pruebas',
+    password: 'magazine',
+  })
+)
+
 // Redirect old WordPress post links
 app.get(
   '/:year{[0-9]{4}}/:month{[0-9]{2}}/:day{[0-9]{2}}/:slug{.+\/?}',
   async (c) => {
-    const slug = c.req.param('slug');
+    let slug = c.req.param('slug');
 
     // Note: Hugo uses pretty urls that end in a trailing slash,
-    // so we add it in the redirect location
-    return c.redirect(`/articulos/${slug}/`, 301);
+    // so we always add it if not present.
+    slug += slug.endsWith("/") ? "" : "/";
+
+    return c.redirect(`/articulos/${slug}`, 301);
   },
 );
 
