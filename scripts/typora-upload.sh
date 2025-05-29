fd7dc51657fecb3b9e2816d97ee8c8fa1665c2a6
germanftorres <703573+germanftorres@users.noreply.github.com>
2025-05-29 14:36:25 +0200

    fixed typora-upload images with spaces

--- scripts/typora-upload.sh
+++ scripts/typora-upload.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
-echo "Subiendo imágenes con rclone a fly.io..."
 for source; do
-   target="fly:fotos.etheriamagazine.com/$(date +%Y/%m)/$(basename $source)"
-   rclone copyto $source $target
-   if [ $? -eq 0 ]
-   then
-      echo ${target/fly:/https://}
-   else
-      echo "Could not create file"
-   fi
+  filename="$(basename "$source")"
+  target="fly:fotos.etheriamagazine.com/$(date +%Y/%m)/$(basename "$source")"
+  rclone copyto "$source" "$target"
+  if [ $? -eq 0 ]
+  then
+    echo ${target/fly:/https://}
+  else
+    echo "Could not create file"
+  fi
 done
