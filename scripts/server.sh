07382b0f71c8dd9dc4c480675e6859afedf3cc25
germanftorres <703573+germanftorres@users.noreply.github.com>
2025-05-30 10:58:30 +0200

    change scripts

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# run hugo dev server
-hugo server
+
+docker run --rm \
+  --name mysite2 \
+  --env-file ${PWD}/.env.local \
+  -v ${PWD}:/src \
+  -p 1313:1313 \
+  hugomods/hugo:latest \
+  hugo server
