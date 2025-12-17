3cac2d97aa45bf63df051a10067003144546709c
germanftorres <703573+germanftorres@users.noreply.github.com>
2025-12-17 20:52:43 +0100

    cambia script server para incluir artículos futuros

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-source .env.local
-
-
-hugo server
-
-# docker run --rm \
-#   --name mysite2 \
-#   --env-file ${PWD}/.env.local \
-#   -v ${PWD}:/src \
-#   -p 1313:1313 \
-#   hugomods/hugo:latest \
-#   hugo server
+hugo server --buildFuture
