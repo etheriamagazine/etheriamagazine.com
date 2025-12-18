fb99733fc1621efdf7a33db985c6de6fdfe64e3b
germanftorres <703573+germanftorres@users.noreply.github.com>
2025-12-18 16:47:06 +0100

    renombra scripts

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# use local dev workspace
-export HUGO_MODULE_WORKSPACE=hugo.work
-
-# build hugo
-hugo
-
-# build pagefind index
-bun run pagefind --site public
-
-# run honojs app
-bun --watch run ./backend/index.ts
+hugo server --buildFuture --buildDrafts
---
+++ scripts/runapp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# use local dev workspace
+export HUGO_MODULE_WORKSPACE=hugo.work
+
+# build hugo
+hugo
+
+# build pagefind index
+bun run pagefind --site public
+
+# run honojs app
+bun --watch run ./backend/index.ts
