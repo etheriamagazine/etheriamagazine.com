9a268d7d4667c549deb66ea3a8e82eb5ba497fc1
germanftorres <703573+germanftorres@users.noreply.github.com>
2026-01-27 18:34:34 +0100

    backend: añade redirección para urls no canónicas de wordpress

--- backend/wordpressRedirect.ts
+++ backend/wordpressRedirect.ts
@@ -1,33 +1,41 @@
 import { Hono } from 'hono';
+import wordpressPosts from './wordpressPosts.json';
+
+// make Typescript happy
+type WordpressPosts = typeof wordpressPosts;
+type PostKey = keyof WordpressPosts | undefined;
 
 const app = new Hono();
 
-// Redirect old WordPress post links
+// Redirect old WordPress link structure
+// => https://etheriamagazine.com/2024/08/08/escapada-con-amigas-a-lisboa-y-ericeira-portugal-surf
 app.get(
   '/:year{[0-9]{4}}/:month{[0-9]{2}}/:day{[0-9]{2}}/:slug{.+\/?}',
   async (c) => {
     let slug = c.req.param('slug');
-
-    // Note: Hugo uses pretty urls that end in a trailing slash,
-    // so we always add it if not present.
-    slug += slug.endsWith("/") ? "" : "/";
-
+    slug += slug.endsWith('/') ? '' : '/'; // add trailing slash for Hugo pretty urls
     return c.redirect(`/articulos/${slug}`, 301);
   },
 );
 
-// Redirect author
-app.get(
-  '/author/:slug{.+\/?}',
-  async (c) => {
-    let slug = c.req.param('slug');
+// Redirect non-canonical WordPress links
+// => https://etheriamagazine.com/?p=67617
+app.get('/', async (c, next) => {
+  const p = c.req.query('p') as PostKey;
 
-    // Note: Hugo uses pretty urls that end in a trailing slash,
-    // so we always add it if not present.
-    slug += slug.endsWith("/") ? "" : "/";
+  if (p && p in wordpressPosts) {
+    let slug = wordpressPosts[p].slug;
+    slug += slug.endsWith('/') ? '' : '/'; // add trailing slash for Hugo pretty urls
+    return c.redirect(`/articulos/${slug}`, 301);
+  }
+  return await next();
+});
 
-    return c.redirect(`/autoras/${slug}`, 301);
-  },
-);
+// Redirect author route to autoras
+app.get('/author/:slug{.+\/?}', async (c) => {
+  let slug = c.req.param('slug');
+  slug += slug.endsWith('/') ? '' : '/';
+  return c.redirect(`/autoras/${slug}`, 301);
+});
 
 export default app;
